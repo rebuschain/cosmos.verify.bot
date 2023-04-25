7bef2715895f24b1b3b8802f5ac20af01e0e9d3b
Ericky Dos Santos <ericky@codeparticle.com>
2023-04-25 11:55:01 -0400

    Support local env file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+dotenv.config();
+dotenv.config({ path: `.env.local`, override: true });
+
 import './discord';
 import './express';
 import { initializeDB } from './db/initialize-db';
